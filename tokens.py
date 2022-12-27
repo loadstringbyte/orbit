f150a545af66669556db580ba1477fe456a4bdc7
cclvi <88312178+loadstringbyte@users.noreply.github.com>
2022-12-26 17:21:13 -0800

    Delete tokens.py

--- tokens.py
+++
@@ -1,7 +0,0 @@
-class Token:
-	def __init__(self, type_, value=None):
-		self.type = type_
-		self.value = value
-	def __repr__(self):
-		if self.value: return "{}:{}".format(self.type, self.value)
-		return '{}'.format(self.type)
